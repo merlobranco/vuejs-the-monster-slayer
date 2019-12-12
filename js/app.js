2fe40886521c6faa0a3b9ccf194288ffb5816686
Brais Cidras <braisomary@gmail.com>
2019-12-12 16:42:33 +0200

    Player and Monster Health style

--- js/app.js
+++ js/app.js
@@ -3,11 +3,33 @@
 new Vue({
 	el: '#app',
 	data: {
-		controls: false
+		controls: false,
+		player: 100,
+		monster: 100,
+		fight: []
 	},
 	methods: {
 		startGame: function() {
 			this.controls = true;
+		}
+	},
+	computed: {
+		healthStyle: function() {
+			return {
+				backgroundColor: 'green', 
+				margin: 0, 
+				color: 'white'
+			}
+		},
+		playerStyle: function() {
+			return {
+				width: this.player + '%'
+			}
+		},
+		monsterStyle: function() {
+			return {
+				width: this.monster + '%'
+			}
 		}
 	}
 });
